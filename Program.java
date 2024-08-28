f8897c57a169bdc3f7ee09abe8d84124f0563d03
aiva4405 <58538705+aiva4405@users.noreply.github.com>
2024-08-28 09:06:45 -0700

    Update Program.java
    almost done, just need to round the output to 2 decimal places

--- Program.java
+++ Program.java
@@ -1,7 +1,57 @@
+package src.main.java;
+
 //import stuff here
-
+import java.util.Scanner;
 //Your code here
-
+public class Program7 {
+    public static void main(String[] args) {
+        // Create variables for scanner, inputs, output & conversion rate
+        Scanner inputScan = new Scanner(System.in);
+        int stanleyNickels;
+        int klevins;
+        int schruteBucks;
+        double decimalStanleys;
+        double decimalKlevins;
+        double outputSchruteBucks;
+        final double conversionRate = 100.0 / 240.0;
+        
+        // Ask for input and store it
+        System.out.print("Enter schrute-bucks: ");
+        
+        schruteBucks = inputScan.nextInt();
+        
+        System.out.print("\nEnter klevins: ");
+        
+        klevins = inputScan.nextInt();
+        
+        System.out.print("\nEnter stanley-nickels: ");
+        
+        stanleyNickels = inputScan.nextInt();
+        
+        // Call conversion functions
+        decimalStanleys = convertStanleys(stanleyNickels, conversionRate);
+        decimalKlevins = convertKlevins(klevins, conversionRate);
+        
+        System.out.println(decimalStanleys + "     " + decimalKlevins);
+        // Create output then output it to the user
+        outputSchruteBucks = (double)schruteBucks + decimalStanleys + decimalKlevins;
+        
+        System.out.print("\nNew schrute-bucks: $" + outputSchruteBucks);
+    }
+    
+    public static double convertStanleys(int stanleys, double rate) {
+        double newStanleys = stanleys * rate;
+        newStanleys /= 100;
+        return newStanleys;
+    }
+    
+    public static double convertKlevins(int klevins, double rate) {
+        double newKlevins = klevins * 12.0;
+        newKlevins *= rate;
+        newKlevins /= 100;
+        return newKlevins;
+    }
+}
 //Paste console output below:
 /*
 
